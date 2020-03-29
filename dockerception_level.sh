a6d449e4fa82484be32937a4064ac4bd301b3021
Ruben Christoffer <rubenc@protonmail.com>
2020-03-29 23:56:04 +0100

    Fixed so that it uses PWD

--- dockerception_level.sh
+++ dockerception_level.sh
@@ -4,4 +4,4 @@ echo "Do you dare go down another level? :O"
 ls -all
 
 NEW_LEVEL=$(($LEVEL + 1))
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v /home/admin/dockerception:/home -e LEVEL=$NEW_LEVEL dockerception
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/home -e LEVEL=$NEW_LEVEL dockerception
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v /home/admin/dockerception:/home -e LEVEL=1 dockerception
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/home -e LEVEL=1 dockerception
